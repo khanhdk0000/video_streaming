f1147a2a76827caa5eef3c2033d91d9393b617ce
gitHub882000 <gitHub882000>
2020-11-12 17:05:50 +0700

    Make command line arg possible

--- VideoEncodingTool.py
+++ VideoEncodingTool.py
@@ -3,7 +3,7 @@
 # into mjpeg video that fits the format of the video
 # given in the source codes.
 ####################################################
-
+import sys
 from functools import reduce
 
 ####################################################
@@ -11,52 +11,57 @@ from functools import reduce
 # Remember the file must be in the same folder with
 # The VideoEncodingTool.py file.
 ####################################################
-fileName = "test.mjpeg"
-file = open(fileName, 'rb')
-data = []
-frameLength = 0
-shouldClose = False
+def main(argv):
+    try:
+        fileName = argv
+        file = open(fileName, 'rb')
+        data = []
+        frameLength = 0
+        shouldClose = False
 
 
-while not shouldClose:
-    while True:
-        bytes_data = b''
-        temp_byte = file.read(1)
-        if temp_byte == b'':
-            shouldClose = True
-            break
-        if temp_byte == b'\xff':
-            temp_byte += file.read(1)
-            if temp_byte == b'\xff\xd8':
-                bytes_data += temp_byte
-                frameLength += 2
-                while True:
-                    temp_byte = file.read(1)
-                    bytes_data += temp_byte
-                    frameLength += 1
+        while not shouldClose:
+            while True:
+                bytes_data = b''
+                temp_byte = file.read(1)
+                if temp_byte == b'':
+                    shouldClose = True
+                    break
+                if temp_byte == b'\xff':
+                    temp_byte += file.read(1)
+                    if temp_byte == b'\xff\xd8':
+                        bytes_data += temp_byte
+                        frameLength += 2
+                        while True:
+                            temp_byte = file.read(1)
+                            bytes_data += temp_byte
+                            frameLength += 1
 
-                    if temp_byte == b'\xff':
-                        res = file.read(1)
-                        temp_byte += res
-                        bytes_data += res
-                        frameLength += 1
-                        if temp_byte == b'\xff\xd9':
-                            byteLength = bytes(str(frameLength), "ascii")
-                            if byteLength > b'99999':
-                                raise Exception(
-                                    "This file's frame length is too big to encode!")
-                            else:
-                                first5Bytes = "{:0>5}".format(int(byteLength))
-                                bytes_data = bytes(first5Bytes, "utf-8") + bytes_data
-                                data.append(bytes_data)
-                                frameLength = 0
-                            break
-                break
-file.close()
+                            if temp_byte == b'\xff':
+                                res = file.read(1)
+                                temp_byte += res
+                                bytes_data += res
+                                frameLength += 1
+                                if temp_byte == b'\xff\xd9':
+                                    byteLength = bytes(str(frameLength), "ascii")
+                                    if byteLength > b'99999':
+                                        raise Exception(
+                                            "This file's frame length is too big to encode!")
+                                    else:
+                                        first5Bytes = "{:0>5}".format(int(byteLength))
+                                        bytes_data = bytes(first5Bytes, "utf-8") + bytes_data
+                                        data.append(bytes_data)
+                                        frameLength = 0
+                                    break
+                        break
+        file.close()
 
-result = reduce(lambda x, y: x + y, data)
-print(result)
+        file = open(fileName, 'wb')
+        file.write(reduce(lambda x, y: x + y, data))
+        file.close()
 
-file = open(fileName, 'wb')
-file.write(result)
-file.close()
+    except:
+        print("Find not found!")
+
+if __name__ == "__main__":
+    main(sys.argv[1])
