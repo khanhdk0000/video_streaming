65c033b7530326b79e7625bcfb448082a879c102
Dang Tran Khanh <57286986+khanhdk0000@users.noreply.github.com>
2020-11-15 23:21:23 +0700

    Update final

--- Client.py
+++ Client.py
@@ -107,8 +107,6 @@ class Client:
                     rtpPacket.decode(data)
 
                     currFrameNbr = rtpPacket.seqNum()
-                    print("Current Seq Num: " + str(currFrameNbr))
-
                     if currFrameNbr > self.frameNbr:  # Discard the late packet
                         self.frameNbr = currFrameNbr
                         self.updateMovie(self.writeFrame(
